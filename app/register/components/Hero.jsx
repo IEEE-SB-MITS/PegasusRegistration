445f06da7577d734cc6fd62bb975d4e0654bb821
xrg360 <code.mastermind.07@gmail.com>
2024-12-17 11:08:49 +0530

    hero component fixedd

--- app/register/components/Hero.jsx
+++ app/register/components/Hero.jsx
@@ -5,7 +5,7 @@ import peg from '@/app/assets/peg.png'
 const Hero = () => {
   return (
     <div className='w-1/2 border-r-[1.5px] border-[#6D6D6D] my-4 hidden md:flex justify-center items-center'>
-      <div className='mt-16 mx-36 fixed top-1/4'><Image src={peg} alt='pegasus' className='object-contain'/></div>
+      <div className='mt-16 mx-36 fixed top-1/4'><Image src={peg} alt='pegasus' className='w-full h-full'/></div>
     </div>
   )
 }
